9c167e42d4c7c0033593d56e5d8dac7e73f91158
David Love <dlove@math.arizona.edu>
2013-02-20 10:27:01 -0700

    Improved argument handling in run_rd.sh
    (1) Listed argument documentation more clearly in comment
    (2) Added fourth argument to give a name to the parsed output file
    (3) Removed old and unneeded comments
    (4) Decreased number of replications to 300

--- run_rd.sh
+++ run_rd.sh
@@ -1,18 +1,20 @@
 #! /bin/bash
 # Runs a loop over multiple values of gamma for the newsvendor problem
-# $1 is the problem number, $2 is the value of m, $3 is k
+# Arguments:
+# $1: Problem Name
+# $2: Value of m -- batch size
+# $3: k -- number of nonoverlapping batches
+# $4: Special designation of output file
 
-# 4/26/12: Removing the below line to just go nonoverlapping batches
-# for gamma in 0001 0005 0010 0020 0025 0033 0050 0067 0075 0100 0125 0133 0150 0167 0200 0225  0250 0300 0333 0350 0375 0400 0450 0500 0550 0600 0650 0667 0750 1000
-for gamma in $2
+for gamma in 0001 0005 0010 0020 0025 0033 0050 0067 0075 0100 0125 0133 0150 0167 0200 0225  0250 0300 0333 0350 0375 0400 0450 0500 0550 0600 0650 0667 0750 1000
 do
    if [ $2 -ge $gamma ];
    then
-      RUN="./decomp testproblems/${1} -s $2 -r 1000 -k $3 -g $gamma | tee $1_m$2_$gamma.dat | grep Rep"
+      RUN="./decomp testproblems/${1} -s $2 -r 300 -k $3 -g $gamma | tee $1_m$2_$gamma.dat | grep Rep"
       echo ${RUN}
       eval ${RUN}
    fi
 done
 
 echo "Done!"
-./parser/parser ${1}_m${2}_*.dat -o motivation_${1}_m${2}.dat
+./parser/parser ${1}_m${2}_*.dat -o ${4}_${1}_m${2}.dat
