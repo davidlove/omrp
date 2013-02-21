4942f12f3b14a60c82bf740876a9ccab7216b16d
David Love <dlove@math.arizona.edu>
2013-02-20 21:57:24 -0700

    run_rd.sh uses gammabar rather than gamma as the loop index
    Gammabar = gamma/n is the relative batch nonoverlap.  The loop in
    run_rd.sh now uses 100*gammabar as the loop counter.  This will make it
    easier to run multiple batch sizes in a reasonable manner

--- run_rd.sh
+++ run_rd.sh
@@ -6,14 +6,12 @@
 # $3: k -- number of nonoverlapping batches
 # $4: Special designation of output file
 
-for gamma in 0001 0005 0010 0020 0025 0033 0050 0067 0075 0100 0125 0133 0150 0167 0200 0225  0250 0300 0333 0350 0375 0400 0450 0500 0550 0600 0650 0667 0750 1000
+for gammabar in 5 10 20 33 50 75 100
 do
-   if [ $2 -ge $gamma ];
-   then
-      RUN="./decomp testproblems/${1} -s $2 -r 300 -k $3 -g $gamma | tee $1_m$2_$gamma.dat | grep Rep"
-      echo ${RUN}
-      eval ${RUN}
-   fi
+   gamma=$[$gammabar*$2/100]
+   RUN="./decomp testproblems/${1} -s $2 -r 300 -k $3 -g $gamma | tee $1_m$2_$gamma.dat | grep Rep"
+   echo ${RUN}
+   eval ${RUN}
 done
 
 echo "Done!"
