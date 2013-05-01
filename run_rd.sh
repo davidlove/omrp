6ea582ce2fb796db7100300478974125fa80d209
David Love <dlove@math.arizona.edu>
2013-05-01 14:53:37 -0700

    Add explicit replications argument to run_rd.sh
    Argument 4 now gives number of replications
    
    Argument 5 now gives the name of the output files

--- run_rd.sh
+++ run_rd.sh
@@ -4,15 +4,16 @@
 # $1: Problem Name
 # $2: Value of m -- batch size
 # $3: k -- number of nonoverlapping batches
-# $4: Special designation of output file
+# $4: r -- number of replications
+# $5: Special designation of output file
 
 for gammabar in 5 10 20 33 50 75 100
 do
    gamma=$[$gammabar*$2/100]
-   RUN="./decomp testproblems/${1} -s $2 -r 300 -k $3 -g $gamma | tee $1_m$2_$gamma.dat | grep Rep"
+   RUN="./decomp testproblems/${1} -s $2 -r $4 -k $3 -g $gamma | tee $1_m$2_$gamma.dat | grep Rep"
    echo ${RUN}
    eval ${RUN}
 done
 
 echo "Done!"
-./parser/parser ${1}_m${2}_*.dat -o ${4}_${1}_m${2}.dat
+./parser/parser ${1}_m${2}_*.dat -o ${5}_${1}_m${2}_r${4}.dat
