8907c7d56533b4b13a92803d079532950c6f83ca
David Love <love.david.k@gmail.com>
2014-06-14 19:52:03 -0400

    Add computational efficiency plot with newsvendor data
    	modified:   comp_efficiency/Run.m

--- comp_efficiency/Run.m
+++ comp_efficiency/Run.m
@@ -1,1 +1,24 @@
-PlotTheory([1, 0.667,0.333,0], 10000)
+figure(1)
+PlotTheory([1, 0.667,0.333,0], 1000)
+
+data = importdata('m1000_insertion.dat', '\t', 2);
+
+gammabar = data.data(:,1);
+m = max(gammabar);
+gammabar = gammabar/m;
+
+vg = data.data(:,7);
+comptime = data.data(:,8);
+
+efficiency = 1./(vg.*comptime);
+efficiency = efficiency / efficiency(end);
+
+p = 0.711; % Power fit found with R
+[theory,gb] = ComputationalEfficiency(p,m,1000*m);
+
+figure(2)
+plot(gammabar, efficiency, 'o', gb, theory,'-', 'LineWidth',2,'MarkerSize',8)
+xlabel('\bar{\gamma|', 'FontSize',16)
+ylabel('Relative Computational Efficiency', 'FontSize',16)
+legend('Newsvendor with Insertion Sort, m=1000', 'Theoretical, p=0.71', 'Location','SouthEast')
+
