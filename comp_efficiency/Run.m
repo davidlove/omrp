3cec60f2595c3ebf789cbf96e915c3d672010bc9
David Love <love.david.k@gmail.com>
2014-07-13 20:49:46 -0400

    Add work increase comparison to computational efficiency calculations

--- comp_efficiency/Run.m
+++ comp_efficiency/Run.m
@@ -27,3 +27,23 @@ legend('NVD with Insertion Sort, m=1000', 'Theoretical, p=0.71', 'Location','Sou
 figure(3)
 CompareNonoverlapping([1, 0.9, 0.71, 0.5, 0.2, 0], 1000, 1000)
 
+figure(4)
+k = 1000;
+T = [0.001;gammabar].^p;
+overlap = T.*NumBatches([0.001;gammabar],m,k*m);
+
+plot(gammabar, (comptime/comptime(end) - 1)*100, 'bo', 'LineWidth',2,'MarkerSize',8)
+
+hold on
+
+plot([0.001;gammabar],(overlap/k-1)*100,'m-', 'LineWidth',2,'MarkerSize',8)
+
+nb_effective = k./RelativeVariance([0.01;gammabar]);
+plot([0.001;gammabar],(nb_effective/k-1)*100,'k-', 'LineWidth',2,'MarkerSize',8)
+
+xlabel('\gamma/m', 'FontSize',16)
+ylabel('Work increase over base MRP (%)', 'FontSize',16)
+ylim([0,200])
+
+legend('NVD with Insertion Sort, m=1000', 'Theoretical, p=0.71', 'Location','NorthEast')
+
